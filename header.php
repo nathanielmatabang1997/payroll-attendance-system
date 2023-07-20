e4fe8a759eb84078b79ea2ff47019d300200e88e
Nathaniel Ursua Matabang <120173148+nathanielmatabang1997@users.noreply.github.com>
2023-07-20 21:57:11 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<title>Attendance and Payroll System</title>
+  	
+  	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+  	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+
+  	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  	<style type="text/css">
+  		.mt20{
+  			margin-top:20px;
+  		}
+  		.result{
+  			font-size:20px;
+  		}
+      .bold{
+        font-weight: bold;
+      }
+  	</style>
+</head>
